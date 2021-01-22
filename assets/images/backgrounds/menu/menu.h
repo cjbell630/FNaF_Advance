3268d665607d54a32e99290038fd703819acdfd5
Connor Bell <cjbell630@users.noreply.github.com>
2021-01-22 02:37:01 -0500

    update images

--- assets/images/backgrounds/menu/menu.h
+++ assets/images/backgrounds/menu/menu.h
@@ -5,11 +5,11 @@
 //
 //	menu, 512x512@4, 
 //	+ palette 16 entries, not compressed
-//	+ 811 tiles (t|f|p reduced) not compressed
+//	+ 814 tiles (t|f|p reduced) not compressed
 //	+ regular map (in SBBs), not compressed, 64x64 
-//	Total size: 32 + 25952 + 8192 = 34176
+//	Total size: 32 + 26048 + 8192 = 34272
 //
-//	Time-stamp: 2021-01-21, 19:03:03
+//	Time-stamp: 2021-01-22, 02:29:18
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -18,8 +18,8 @@
 #ifndef GRIT_MENU_H
 #define GRIT_MENU_H
 
-#define menuTilesLen 25952
-extern const unsigned short menuTiles[12976];
+#define menuTilesLen 26048
+extern const unsigned short menuTiles[13024];
 
 #define menuMapLen 8192
 extern const unsigned short menuMap[4096];
