efa6e3be1569359405c938db3a03d393663ade22
Connor Bell <ronnoclleb@gmail.com>
2024-09-04 15:57:23 -0400

    create equipment name enum

--- source/game/control/equipment.h
+++ source/game/control/equipment.h
@@ -3,11 +3,16 @@
 
 #include "tonc_types.h"
 
+enum EquipmentNames {
+    RIGHT_DOOR, LEFT_DOOR, RIGHT_LIGHT, LEFT_LIGHT, CAMERA
+};
+
 struct EquipmentWrapper {
     //void (*update)(int frame_num);
     void (*on_night_start)(int night_num);
-    void (*toggle_right_door)();
     byte (*get_usage)();
+    bool (*is_on)(enum EquipmentNames target);
+    bool (*toggle)(enum EquipmentNames target);
 };
 
 extern struct EquipmentWrapper Equipment;
