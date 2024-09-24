cf2c2a01da6284f4d3488b8c23197d70ebe2ebf8
Connor Bell <ronnoclleb@gmail.com>
2024-09-24 18:56:06 -0400

    create left door image

---
+++ assets/images/office/door/l_door.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(l_door)
+
+//======================================================================
+//
+//	l_door, 64x2400@4, 
+//	+ palette 16 entries, not compressed
+//	+ 2400 tiles not compressed
+//	Total size: 32 + 76800 = 76832
+//
+//	Time-stamp: 2024-09-24, 18:53:41
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_L_DOOR_H
+#define GRIT_L_DOOR_H
+
+#define l_doorTilesLen 76800
+extern const unsigned short l_doorTiles[38400];
+
+#define l_doorPalLen 32
+extern const unsigned short l_doorPal[16];
+
+#endif // GRIT_L_DOOR_H
+
+//}}BLOCK(l_door)
