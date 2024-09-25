0693f72fe07a77943da6fe7397240fdf193e0dcf
Connor Bell <ronnoclleb@gmail.com>
2024-09-25 15:44:36 -0400

    create right door sprites

---
+++ assets/images/office/door/r_door.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(r_door)
+
+//======================================================================
+//
+//	r_door, 64x2400@4, 
+//	+ palette 16 entries, not compressed
+//	+ 2400 tiles not compressed
+//	Total size: 32 + 76800 = 76832
+//
+//	Time-stamp: 2024-09-25, 15:22:26
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_R_DOOR_H
+#define GRIT_R_DOOR_H
+
+#define r_doorTilesLen 76800
+extern const unsigned short r_doorTiles[38400];
+
+#define r_doorPalLen 32
+extern const unsigned short r_doorPal[16];
+
+#endif // GRIT_R_DOOR_H
+
+//}}BLOCK(r_door)
