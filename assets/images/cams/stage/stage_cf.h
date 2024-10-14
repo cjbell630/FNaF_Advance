5e06e37c8303af8952e7181388748e412ac69f3f
Connor Bell <ronnoclleb@gmail.com>
2024-10-14 18:38:57 -0400

    fix stage chica freddy image

--- assets/images/cams/stage/stage_cf.h
+++ assets/images/cams/stage/stage_cf.h
@@ -4,12 +4,12 @@
 //======================================================================
 //
 //	stage_cf, 512x256@8, 
-//	+ palette 200 entries, not compressed
-//	+ 569 tiles (t|f|p reduced) not compressed
+//	+ palette 202 entries, not compressed
+//	+ 573 tiles (t|f|p reduced) not compressed
 //	+ regular map (in SBBs), not compressed, 64x32 
-//	Total size: 400 + 36416 + 4096 = 40912
+//	Total size: 404 + 36672 + 4096 = 41172
 //
-//	Time-stamp: 2024-10-13, 15:13:45
+//	Time-stamp: 2024-10-14, 18:36:51
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -18,14 +18,14 @@
 #ifndef GRIT_STAGE_CF_H
 #define GRIT_STAGE_CF_H
 
-#define stage_cfTilesLen 36416
-extern const unsigned short stage_cfTiles[18208];
+#define stage_cfTilesLen 36672
+extern const unsigned short stage_cfTiles[18336];
 
 #define stage_cfMapLen 4096
 extern const unsigned short stage_cfMap[2048];
 
-#define stage_cfPalLen 400
-extern const unsigned short stage_cfPal[200];
+#define stage_cfPalLen 404
+extern const unsigned short stage_cfPal[202];
 
 #endif // GRIT_STAGE_CF_H
 
