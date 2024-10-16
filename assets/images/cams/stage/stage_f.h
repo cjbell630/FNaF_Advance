288550b4d992ab2b072ddf23ecde31833100a3b6
Connor Bell <ronnoclleb@gmail.com>
2024-10-15 21:22:50 -0400

    fix coloring error with freddy alone on stage

--- assets/images/cams/stage/stage_f.h
+++ assets/images/cams/stage/stage_f.h
@@ -4,12 +4,12 @@
 //======================================================================
 //
 //	stage_f, 512x256@8, 
-//	+ palette 137 entries, not compressed
-//	+ 767 tiles (t|f|p reduced) not compressed
+//	+ palette 138 entries, not compressed
+//	+ 767 tiles (t|f reduced) not compressed
 //	+ regular map (in SBBs), not compressed, 64x32 
-//	Total size: 274 + 49088 + 4096 = 53458
+//	Total size: 276 + 49088 + 4096 = 53460
 //
-//	Time-stamp: 2024-10-13, 15:15:25
+//	Time-stamp: 2024-10-15, 21:20:55
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -24,7 +24,7 @@ extern const unsigned short stage_fTiles[24544];
 #define stage_fMapLen 4096
 extern const unsigned short stage_fMap[2048];
 
-#define stage_fPalLen 274
+#define stage_fPalLen 276
 extern const unsigned short stage_fPal[138];
 
 #endif // GRIT_STAGE_F_H
